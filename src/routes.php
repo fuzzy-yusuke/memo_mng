6060f504a52937cb1ee50d04953c4cfa08137ee1
fuzzy-yusuke <youth_k0228@outlook.com>
2020-09-04 23:07:23 +0900

    ルーティングの設定

--- src/routes.php
+++ src/routes.php
@@ -7,6 +7,36 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
+    //一覧表示（トップページ）
+    $app->get('/memos',function(Request $request, Response $response){
+    });
+
+    //新規作成
+    $app->get('/memo/new',function(Request $request, Response $response){
+    });
+
+    //保存（新規作成語に実行される。登録）
+    $app->post('/memo',function(Request $request, Response $response){
+    });
+
+    //詳細ページ（以下、特定の投稿に対しての処理になるので、「array $args」でidを指定する。）
+    $app->get('/memo/{id}',function(Request $request, Response $response, array $args){
+    });
+
+    //編集
+    $app->get('/memo/edit/{id}',function(Request $request, Response $response, array $args){
+    });
+
+    //更新（機能自体は「保存」と同じだが、部分的な変更に対して実行される為、「patch」を使う。）
+    $app->patch('/memo/update/{id}',function(Request $request, Response $response, array $args){
+    });
+
+    //削除
+    $app->delete('/memo/{id}',function(Request $request, Response $response, array $args){
+    });
+
+
+
     $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
