c6690d3fb7d1593820b6532208437cb87e5b3cf0
fuzzy-yusuke <youth_k0228@outlook.com>
2020-09-09 00:18:34 +0900

    一覧表示のビューを作成

--- templates/memos/new.php
+++ templates/memos/new.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>メモ管理</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <div class="container">
